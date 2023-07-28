382611bb8dfe54dfae69f3b3afb634759a340a82
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-07-28 14:36:35 -0700

    Copy in sample code.

--- CS131-CoinFlip/CS131-CoinFlip.cpp
+++ CS131-CoinFlip/CS131-CoinFlip.cpp
@@ -24,8 +24,17 @@ string CoinFlip()
 */
 
 #include <iostream>
+#include <cstdlib>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+/* Define your function here */
+
+int main() {
+	// Add more variables as needed
+
+	srand(2);  // Unique seed
+
+	/* Type your code here */
+
+	return 0;
 }
